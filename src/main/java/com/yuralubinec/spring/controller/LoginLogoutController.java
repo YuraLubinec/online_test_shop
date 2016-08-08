696062d549f7591f415b23e6a97ec94fdeeb6154
Yura Lubinec <yuralubinec@gmail.com>
2016-08-09 02:34:36 +0300

    added spring security configuration file and error403 handler to controller

--- src/main/java/com/yuralubinec/spring/controller/LoginLogoutController.java
+++ src/main/java/com/yuralubinec/spring/controller/LoginLogoutController.java
@@ -2,10 +2,19 @@ package com.yuralubinec.spring.controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginLogoutController {
     
     private static final Logger LOGGER = Logger.getLogger(LoginLogoutController.class); 
-
+    
+    
+    @RequestMapping(value = "/403", method = RequestMethod.GET)
+    public String error403(Model model) {
+        model.addAttribute("message", "You don't have permission to access this page");
+        return "error";
+    }
 }
